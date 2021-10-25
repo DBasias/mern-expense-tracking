d71bac305658d98486bb1035a169428ee7275482
DBasias <dbasiadimas@hotmail.com>
2021-10-25 14:37:41 +0300

    created model for expenses

---
+++ server/models/expense.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const ExpenseSchema = new mongoose.Model({
+  title: {
+    type: String,
+    trim: true,
+    required: "Title is required",
+  },
+  amount: {
+    type: Number,
+    min: 0,
+    required: "Amount is required",
+  },
+  category: {
+    type: String,
+    trim: true,
+    required: "Category is required",
+  },
+  incurred_on: {
+    type: Date,
+    default: Date.now,
+  },
+  notes: {
+    type: String,
+    trim: true,
+  },
+  recorded_by: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+  updated: Date,
+});
+
+export default mongoose.model("Expense", ExpenseSchema);
